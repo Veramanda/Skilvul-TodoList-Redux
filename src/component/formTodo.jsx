5b671e797fd7643d81ef7b9bc7e0349c919dde73
Veramanda <faidatun237@gmail.com>
2023-10-30 12:55:15 +0700

    inisialisasi dari reducer

--- src/component/formTodo.jsx
+++ src/component/formTodo.jsx
@@ -1,7 +1,77 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { addTodo, editTodo, deleteTodo, checkboxTodo, all, completed, incomplete } from '../redux/reducer/actions';
+
+
 function FormTodo() {
+  const [newTodo, setNewTodo] = useState('');
+  const [editId, setEditId] = useState(null);
+  const [editTitle, setEditTitle] = useState('');
+  const dispatch = useDispatch();
+  const todos = useSelector((state) => state.todos);
+  const filter = useSelector((state) => state.filter);
+
+  const handleAdd = (e) => {
+    e.preventDefault();
+    if (newTodo.trim() !== '') {
+      dispatch(
+        addTodo({
+          id: Math.floor(Math.random() * 100),
+          title: newTodo,
+          isDone: false,
+        })
+      );
+    }
+  };
+  
+  const handleInput = (e) => {
+    setNewTodo(e.target.value);
+  };
+
+  const handleInputEdit = (e) => {
+    setEditTitle(e.target.value);
+  };
+
+  const handleEdit = (item) => {
+    setEditId(item.id);
+    setEditTitle(item.title);
+  };
+
+  const handleDelete = (id) => {
+    dispatch(deleteTodo(id));
+  };
+
+  const handleCheckbox = (id) => {
+    dispatch(checkboxTodo(id));
+  };
+  const handleAll = () => {
+    dispatch(all());
+  };
+
+  const handleCompleted = () => {
+    dispatch(completed());
+  };
+
+  const handleIncomplete = () => {
+    dispatch(incomplete());
+  };
+
+  const updateTodo = () => {
+    if (editTitle.trim() !== '') {
+      dispatch(
+        editTodo({
+          id: editId,
+          title: editTitle,
+        })
+      );
+      setEditId(null);
+      setEditTitle('');
+    }
+  };
+
   return (
     <>
-      <form >
+      <form  onChange={handleInput} value={newTodo}>
         <div className="grid sm:grid-cols-4 gap-2 sm:gap-4 py-4 px-[6px] m-3">
           <div className="relative w-full sm:col-span-3 gap-x-3">
               <input 
@@ -10,18 +80,19 @@ function FormTodo() {
                 required/>
           </div>
           <button  
-            className="bg-cyan-500 font-semibold rounded-md text-white relative w-full py-2.5">ADD</button>
+            className="bg-cyan-500 font-semibold rounded-md text-white relative w-full py-2.5"
+            onClick={handleAdd}>ADD</button>
         </div> 
       </form>
 
       <div className="pb-2">
-        <button className="px-2 py-[4px] m-2 bg-cyan-500 text-white rounded-md font-semibold">
+        <button className="px-2 py-[4px] m-2 bg-cyan-500 text-white rounded-md font-semibold" onClick={handleAll}>
           All
         </button>
-        <button className="px-2 py-[4px] m-2 bg-cyan-500 text-white rounded-md font-semibold">
+        <button className="px-2 py-[4px] m-2 bg-cyan-500 text-white rounded-md font-semibold" onClick={handleIncomplete}>
           Incomplete
         </button>
-        <button className="px-2 py-[4px] m-2 bg-cyan-500 text-white rounded-md font-semibold">
+        <button className="px-2 py-[4px] m-2 bg-cyan-500 text-white rounded-md font-semibold" onClick={handleCompleted}>
           Completed
         </button>
       </div>
